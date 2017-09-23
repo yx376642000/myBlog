66c806f74dedffef77207e435bacc9e7e08dd63b
yx376642000 <30583662+yx376642000@users.noreply.github.com>
2017-09-23 22:54:21 +0800

    Update admin.js

--- routers/admin.js
+++ routers/admin.js
@@ -4,14 +4,14 @@ var User = require("../models/User");
 var Category = require("../models/Category");
 var Article = require("../models/Article");
 
-router.use(function (req,res,next) {
-   if(!req.userInfo.isAdmin){
+//router.use(function (req,res,next) {
+  // if(!req.userInfo.isAdmin){
        //如果当前用户为非管理员
-       res.send("对不起，只有管理员才能够进入后台管理页面！");
-       return;
-   }
-   next();
-});
+      // res.send("对不起，只有管理员才能够进入后台管理页面！");
+      // return;
+   //}
+  // next();
+//});
 /*
 * 首页
 * */
